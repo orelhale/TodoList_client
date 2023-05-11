17d00cb6f7e5a29694efc1a7248dcacfd9fa6484
orelhale <oreljbh@gmail.com>
2023-05-11 12:52:17 +0300

    clean file

--- src/layout/Layout/index.jsx
+++ src/layout/Layout/index.jsx
@@ -1,35 +1,8 @@
 import Header from "../Header"
 import Main from "../Main"
 import Nav from "../Nav"
-import Context from "../../context/Context"
-import { useContext, useEffect } from "react"
-import { checkToken, setToken, deleteToken } from "../../functions/tokenFunctions"
-import { useNavigate } from "react-router-dom"
 
 export default function Layout() {
-	
-	// let { userData, setUserData } = useContext(Context)
-	// let navigate = useNavigate()
-
-
-	// useEffect(() => {
-	// 	startCheckToken()
-	// }, [])
-
-
-	// async function startCheckToken() {
-	// 	return;
-	// 	let dataFromUser = await checkToken();
-
-	// 	if (dataFromUser) {
-	// 		setUserData(dataFromUser)
-	// 	} else {
-	// 		deleteToken()
-	// 		setUserData(null)
-	// 		navigate("login")
-	// 	}
-	// }
-
 
 	return (
 		<div className="Layout">
--- src/routes/SystemRoutes/index.jsx
+++ src/routes/SystemRoutes/index.jsx
@@ -1,13 +1,8 @@
 import { Route, Routes } from "react-router-dom";
-import Login from "../../pages/Login";
-import Register from "../../pages/Register";
-
 
 export default function SystemRoutes({ children }) {
 	return (
 		<Routes>
-			<Route path="login" element={<Login />} />
-			<Route path="register" element={<Register />} />
 			<Route path="*" element={children} />
 		</Routes>
 	)
